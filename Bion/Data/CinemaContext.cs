6be2257f3d2cff481871704c3b484b5547f10cad
lobba111 <robinolsson@gmail.com>
2022-04-25 16:22:56 +0200

    ajdbs

---
+++ Bion/Data/CinemaContext.cs
@@ -0,0 +1,13 @@
+﻿using Bion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bion.Data
+{
+    public class CinemaContext : DbContext
+    {
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options) { }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<MovieShowTime> MovieShowTimes { get; set; }
+    }
+}
--- Bion/Program.cs
+++ Bion/Program.cs
@@ -1,7 +1,14 @@
+using Bion.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<CinemaContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
